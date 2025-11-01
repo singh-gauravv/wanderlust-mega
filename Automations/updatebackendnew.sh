4e7b91fbff21a06f6ae8093d1e92f0de8b81b574
Gaurav Singh <gauravsunil2005@gmail.com>
2025-11-01 20:31:26 +0530

    Automations of frontend and backend added

---
+++ Automations/updatebackendnew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+INSTANCE_ID="i-030da7d31a1dbbffc"
+
+
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+
+file_to_find="../backend/.env.docker"
+
+
+current_url=$(sed -n "4p" $file_to_find)
+
+
+if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
+    if [ -f $file_to_find ]; then
+        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+    else
+        echo "ERROR: File not found."
+    fi
+fi
---
+++ Automations/updatefrontendnew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+INSTANCE_ID="i-030da7d31a1dbbffc"
+
+
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+
+file_to_find="../frontend/.env.docker"
+
+
+current_url=$(cat $file_to_find)
+
+
+if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
+    if [ -f $file_to_find ]; then
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
+    else
+        echo "ERROR: File not found."
+    fi
+fi
